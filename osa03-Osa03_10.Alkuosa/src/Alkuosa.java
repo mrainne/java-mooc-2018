4387669ed6d323522249553d54ca99d7d7170306
M Tamminen <21221663+mrainne@users.noreply.github.com>
2018-10-09 12:01:39 +0300

    add Alkuosa.java

--- osa03-Osa03_10.Alkuosa/src/Alkuosa.java
+++ osa03-Osa03_10.Alkuosa/src/Alkuosa.java
@@ -5,5 +5,11 @@ public class Alkuosa {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
+        System.out.print("Anna sana: ");
+        String sana = lukija.nextLine();
+        System.out.print("Alkuosan pituus: ");
+        int pituus  = Integer.parseInt(lukija.nextLine());
+        
+        System.out.println("Tulos: " + sana.substring(0, pituus));
     }
 }
