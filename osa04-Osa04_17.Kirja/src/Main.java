13d4d97c298e953179d65a77c6eadc96d83d6ab0
M Tamminen <21221663+mrainne@users.noreply.github.com>
2018-10-11 21:40:57 +0300

    add Kirja.java, modify Main.java

--- osa04-Osa04_17.Kirja/src/Main.java
+++ osa04-Osa04_17.Kirja/src/Main.java
@@ -5,8 +5,8 @@ public class Main {
         // Tämä on vain tyhjä main-metodi jossa voit kokeilla
         // Kirja-luokkaasi. Kokeile esim:
 
-//        Kirja k = new Kirja("J. K. Rowling", "Harry Potter ja viisasten kivi", 223);
-//        System.out.println(k);
+        Kirja k = new Kirja("J. K. Rowling", "Harry Potter ja viisasten kivi", 223);
+        System.out.println(k);
 
     }
 }
