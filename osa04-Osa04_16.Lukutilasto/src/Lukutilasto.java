d009f31984af415c9ed583d07075cddae017740e
M Tamminen <21221663+mrainne@users.noreply.github.com>
2018-10-11 21:31:13 +0300

    modify Lukutilasto.java and Paaohjelma.java

--- osa04-Osa04_16.Lukutilasto/src/Lukutilasto.java
+++ osa04-Osa04_16.Lukutilasto/src/Lukutilasto.java
@@ -1,3 +1,31 @@
 
 public class Lukutilasto {
+    private int lukujenMaara;
+    private int summa;
+    
+    public Lukutilasto(){
+        this.lukujenMaara = 0;
+        this.summa = 0;
+    }
+    
+    public void lisaaLuku(int luku) {
+        this.lukujenMaara++;
+        this.summa += luku;
+    }
+    
+    public int haeLukujenMaara() {
+        return this.lukujenMaara;
+    }
+    
+    public int summa() {
+        return this.summa;
+    }
+    
+    public double keskiarvo() {
+        if (this.lukujenMaara != 0) {
+            return 1.0 * this.summa / this.lukujenMaara;
+        } else {
+            return 0;
+        }
+    }
 }
--- osa04-Osa04_16.Lukutilasto/src/Paaohjelma.java
+++ osa04-Osa04_16.Lukutilasto/src/Paaohjelma.java
@@ -11,5 +11,26 @@ public class Paaohjelma {
         // Jotta testi toimisi, on oliot luotava pääohjelmassa oikeassa järjestyksessä 
         //  eli ensin kaikkien summan laskeva olio, toisena parillisten summan laskeva 
         //  ja viimeisenä parittomien summan laskeva olio)!
+        Lukutilasto tilasto = new Lukutilasto();
+        Lukutilasto parilliset = new Lukutilasto();
+        Lukutilasto parittomat = new Lukutilasto();
+        System.out.println("Anna lukuja:");
+        while (true) {   
+            int luku = Integer.parseInt(lukija.nextLine());
+            
+            if (luku == -1) {
+                break;
+            }
+            tilasto.lisaaLuku(luku);
+            
+            if (luku % 2 == 0) {
+                parilliset.lisaaLuku(luku);
+            } else {
+                parittomat.lisaaLuku(luku);
+            }
+        }
+        System.out.println("Summa: " + tilasto.summa());
+        System.out.println("Parillisten summa: " + parilliset.summa());
+        System.out.println("Parittomien summa: " + parittomat.summa());
     }
 }
