5aa6771f78a7192dd91a0fe66bc4ff03917f7698
M Tamminen <21221663+mrainne@users.noreply.github.com>
2018-10-11 19:01:49 +0300

    add EnsimmainenTilisi.java

--- osa04-Osa04_01.EnsimmainenTilisi/src/EnsimmainenTilisi.java
+++ osa04-Osa04_01.EnsimmainenTilisi/src/EnsimmainenTilisi.java
@@ -4,5 +4,8 @@ public class EnsimmainenTilisi {
     public static void main(String[] args) {
         // Tili.Java:ssa olevaan koodiin ei tule koskea
         // tee ohjelmasi tänne
+        Tili munTili = new Tili("M", 100.0);
+        munTili.pano(20.0);
+        System.out.println(munTili);
     }
 }
