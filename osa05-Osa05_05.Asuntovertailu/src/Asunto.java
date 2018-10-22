bf694ad05190901392181471d946f0bcc8f5b6de
M Tamminen <21221663+mrainne@users.noreply.github.com>
2018-10-22 20:03:54 +0300

    Add Asunto.java and Main.java

---
+++ osa05-Osa05_05.Asuntovertailu/src/Asunto.java
@@ -0,0 +1,25 @@
+
+public class Asunto {
+
+    private int huoneita;
+    private int nelioita;
+    private int neliohinta;
+
+    public Asunto(int huoneita, int nelioita, int neliohinta) {
+        this.huoneita = huoneita;
+        this.nelioita = nelioita;
+        this.neliohinta = neliohinta;
+    }
+
+    public boolean suurempi(Asunto verrattava) {
+        return (this.nelioita > verrattava.nelioita);
+    }
+    
+    public int hintaero(Asunto verrattava) {
+        return Math.abs(this.nelioita * this.neliohinta - verrattava.nelioita * verrattava.neliohinta);
+    }
+    
+    public boolean kalliimpi(Asunto verrattava) {
+        return (this.nelioita * this.neliohinta > verrattava.nelioita * verrattava.neliohinta);
+    }
+}
