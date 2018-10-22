d48e8f228ac43f4eec42826fc7def3791175aab8
M Tamminen <21221663+mrainne@users.noreply.github.com>
2018-10-22 19:50:18 +0300

    Add Kassapaate.java, Maksukortti.java and Paaohjelma.java

---
+++ osa05-Osa05_04.MaksukorttiJaKassapaate/src/Kassapaate.java
@@ -0,0 +1,80 @@
+
+public class Kassapaate {
+
+
+    private double rahaa;  // kassassa olevan käteisen määrä
+    private int edulliset; // myytyjen edullisten lounaiden määrä
+    private int maukkaat;  // myytyjen maukkaiden lounaiden määrä
+
+
+    public Kassapaate() {
+        // kassassa on aluksi 1000 euroa rahaa
+        this.rahaa = 1000.0;
+        this.edulliset = 0;
+        this.maukkaat = 0;
+    }
+
+    public double syoEdullisesti(double maksu) {
+        // edullinen lounas maksaa 2.50 euroa.
+        // päivitetään kassan rahamäärää lounaan hinnalla ja palautetaan vaihtorahat
+        // jos parametrina annettu maksu ei ole riittävän suuri, ei lounasta myydä ja metodi palauttaa koko summan
+        if (maksu >= 2.50) {
+            this.rahaa += 2.50;
+            this.edulliset++;
+            return (maksu - 2.50);
+        }
+        return maksu;
+    }
+
+    public double syoMaukkaasti(double maksu) {
+        // edullinen lounas maksaa 4.30 euroa.
+        // päivitetään kassan rahamäärää lounaan hinnalla ja palautetaan vaihtorahat
+        // jos parametrina annettu maksu ei ole riittävän suuri, ei lounasta myydä ja metodi palauttaa koko summan
+        if (maksu >= 4.30) {
+            this.rahaa += 4.30;
+            this.maukkaat++;
+            return (maksu - 4.30);
+        }
+        return maksu;
+    }
+    
+    public boolean syoEdullisesti(Maksukortti kortti) {
+        // edullinen lounas maksaa 2.50 euroa.
+        // jos kortilla on tarpeeksi rahaa, vähennetään hinta kortilta ja palautetaan true
+        // muuten palautetaan false
+        if (kortti.saldo() >= 2.50) {
+            kortti.otaRahaa(2.50);
+            this.edulliset++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean syoMaukkaasti(Maksukortti kortti) {
+        // maukas lounas maksaa 4.30 euroa.
+        // jos kortilla on tarpeeksi rahaa, vähennetään hinta kortilta ja palautetaan true
+        // muuten palautetaan false
+        if (kortti.saldo() >= 4.30) {
+            kortti.otaRahaa(4.30);
+            this.maukkaat++;
+            return true;
+        }
+        return false;
+    }
+
+    public void lataaRahaaKortille(Maksukortti kortti, double summa) {
+        // ladtaan kortille rahaa määrä summa ja lisätään summa myös
+        // käteiskassaan
+        if (summa >= 0) {
+            this.rahaa += summa;
+            kortti.lataaRahaa(summa);
+        }
+        
+    }
+
+    @Override
+    public String toString() {
+        return "kassassa rahaa " + rahaa + " edullisia lounaita myyty " +
+               edulliset + " maukkaita lounaita myyty " + maukkaat;
+    }
+}
