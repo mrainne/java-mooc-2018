e40d23cd16ee2d9d11f8583cb3da30a1519ce431
M Tamminen <21221663+mrainne@users.noreply.github.com>
2018-10-28 20:52:30 +0200

    Add LoytyykoTiedostosta.java and text files nimet.txt toiset-nimet.txt

---
+++ osa06-Osa06_04.LoytyykoTiedostosta/src/LoytyykoTiedostosta.java
@@ -0,0 +1,34 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class LoytyykoTiedostosta {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        System.out.println("Minkä niminen tiedosto luetaan? ");
+        String tiedosto = lukija.nextLine();
+        ArrayList<String> lista = new ArrayList<>();
+        
+        try {
+            Scanner tiedostolukija = new Scanner(new File(tiedosto));
+            while (tiedostolukija.hasNextLine()) {
+                lista.add(tiedostolukija.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Tiedoston " + tiedosto + " lukeminen epäonnistui.");
+        }
+
+        System.out.println("Mitä etsitään?");
+        String etsittava = lukija.nextLine();
+        if (lista.contains(etsittava)) {
+            System.out.println("Löytyi!");
+        } else {
+            System.out.println("Ei löytynyt.");
+        }
+
+    }
+}
