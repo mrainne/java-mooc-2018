4d4841bdd78d64e56772d787701aadf2e9fdcb51
M Tamminen <21221663+mrainne@users.noreply.github.com>
2018-10-28 20:37:39 +0200

    Add VieraslistaTiedostosta.java and nimet.txt, toiset-nimet.txt, vieraslista.txt

---
+++ osa06-Osa06_03.VieraslistaTiedostosta/src/VieraslistaTiedostosta.java
@@ -0,0 +1,46 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class VieraslistaTiedostosta {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        System.out.println("Minkä niminen tiedosto luetaan? ");
+        String tiedosto = lukija.nextLine();
+
+        ArrayList<String> lista = new ArrayList<>();
+        // toteuta tiedoston lukeminen tässä.
+        try {
+            Scanner tiedostolukija = new Scanner(new File(tiedosto));
+            while (tiedostolukija.hasNextLine()) {
+                lista.add(tiedostolukija.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Virhe: " + e.getMessage());
+        }
+        
+        System.out.println("");
+
+        System.out.println("Syötä nimiä, tyhjä rivi lopettaa.");
+        while (true) {
+            String nimi = lukija.nextLine();
+            if (nimi.isEmpty()) {
+                break;
+            }
+
+            if (lista.contains(nimi)) {
+                System.out.println("Nimi on listalla.");
+            } else {
+                System.out.println("Nimi ei ole listalla.");
+            }
+        }
+
+        System.out.println("Kiitos!");
+    }
+}
