19dd6579a6acd66668ef88309ea1fb261853dfd2
M Tamminen <21221663+mrainne@users.noreply.github.com>
2018-10-31 17:11:11 +0200

    Add Velkakirja.java and Main.java

---
+++ osa06-Osa06_09.Velkakirja/src/Velkakirja.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ */
+public class Velkakirja {
+    private HashMap<String, Double> velkakirja;
+    
+    public Velkakirja() {
+        this.velkakirja = new HashMap<>();        
+    }
+    
+    public void asetaLaina(String kenelle, double maara) {
+        this.velkakirja.put(kenelle, maara);
+    }
+    
+    public double paljonkoVelkaa(String kuka) {
+        return this.velkakirja.getOrDefault(kuka, 0.0);
+    }   
+}
