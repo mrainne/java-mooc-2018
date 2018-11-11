ee38556edabfcfa77cb5e35e27247443eea78fba
M Tamminen <21221663+mrainne@users.noreply.github.com>
2018-11-11 20:17:12 +0200

    Add Main.java

--- kertaus1-Kertaus01_13.LoytyykoLuvutValilta/src/Main.java
+++ kertaus1-Kertaus01_13.LoytyykoLuvutValilta/src/Main.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
         // Kirjoita ohjelmasi tähän alle
-
+        ArrayList<Integer> luvut = new ArrayList<>();
+        luvut.add(1);
+        luvut.add(4);
+        luvut.add(5);
+        luvut.add(2);
+        luvut.add(3);
+        luvut.add(6);
+        
+        System.out.println("Löytyykö luvut väliltä 4-8 (ei): " + loytyykoLuvutValilta(luvut, 4, 8));
+        System.out.println("Löytyykö luvut väliltä 2-5 (kyllä) " + loytyykoLuvutValilta(luvut, 2, 5));
     }
 
+    public static boolean loytyykoLuvutValilta(ArrayList<Integer> luvut, int alku, int loppu) {
+        ArrayList<Integer> luvutAlustaLoppuun = new ArrayList<>();
+        for (int i = alku; i <= loppu; i++) {
+            luvutAlustaLoppuun.add(i);
+        }
+        
+        return luvut.containsAll(luvutAlustaLoppuun);
+    }
 }
