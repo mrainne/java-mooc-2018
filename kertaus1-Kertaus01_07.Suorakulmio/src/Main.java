286046dac223a6bfe6e78784c9dafadbbf042f79
M Tamminen <21221663+mrainne@users.noreply.github.com>
2018-11-11 19:58:40 +0200

    Add Main.java

--- kertaus1-Kertaus01_07.Suorakulmio/src/Main.java
+++ kertaus1-Kertaus01_07.Suorakulmio/src/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // Kirjoita ohjelmasi tähän alle
-
+        Scanner lukija = new Scanner(System.in);
+        System.out.print("Leveys: ");
+        int leveys = Integer.parseInt(lukija.nextLine());
+        System.out.print("Korkeus: ");
+        int korkeus = Integer.parseInt(lukija.nextLine());
+        
+        
+        for (int i = 1; i <= korkeus; i++) {
+            String xRivi = "";
+            for (int j=1; j <= leveys; j++) {
+                xRivi += String.join("", "x");
+            }
+            System.out.println(xRivi);
+        }
     }
 
 }
