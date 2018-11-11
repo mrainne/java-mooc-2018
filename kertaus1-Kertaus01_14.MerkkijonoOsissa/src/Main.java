0a25adf3241d211aee78134d827acf4d0c435834
M Tamminen <21221663+mrainne@users.noreply.github.com>
2018-11-11 20:18:54 +0200

    Add Main.java

--- kertaus1-Kertaus01_14.MerkkijonoOsissa/src/Main.java
+++ kertaus1-Kertaus01_14.MerkkijonoOsissa/src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // Kirjoita ohjelmasi tähän alle
-
+        Scanner lukija = new Scanner(System.in);
+        System.out.print("Merkkijono: ");
+        String merkkijono = lukija.nextLine();
+        
+        for (int i = merkkijono.length() - 1; i >= 0; i--) {
+            System.out.println(merkkijono.charAt(i));
+        }
     }
 
 }
